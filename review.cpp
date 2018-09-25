64ab1c5354fbc9dd08f8a9de7953c60192e4bc66
yupengzzz <39612917+yupengzzz@users.noreply.github.com>
2018-09-25 08:29:00 -0700

    Update review.cpp

--- review.cpp
+++ review.cpp
@@ -29,14 +29,47 @@ int main()
     cin >> c2;
     cin.ignore(100, '\n');
     
-    if((c1 == 'R' || c1 == 'r') && (c2 == 'R' || c2 == 'r'))
+    if(((c1 == 'R' || c1 == 'r') && (c2 == 'R' || c2 == 'r'))
+       || ((c1 == 'P' || c1 == 'p') && (c2 == 'P' || c2 == 'p'))
+       || ((c1 == 'S' || c1 == 's') && (c2 == 'S' || c2 == 's')))
     {
       cout << "Draw. Let's play another round." << endl;
+      continue;
+    }
+    elseif((c1 == 'R' || c1 == 'r') && (c2 == 'P' || c2 == 'p'))
+    {
+      cout << "Paper wraps rock, player 2 wins!!" << endl;
+    }
+    elseif((c1 == 'R' || c1 == 'r') && (c2 == 'S' || c2 == 's'))
+    {
+      cout << "Rock smashes scissor, player 1 wins!!" << endl;
+    }
+    elseif((c1 == 'P' || c1 == 'p') && (c2 == 'R' || c2 == 'r'))
+    {
+      cout << "Paper wraps rock, player 1 wins!!" << endl;
+    }
+    elseif((c1 == 'P' || c1 == 'p') && (c2 == 'S' || c2 == 's'))
+    {
+      cout << "Scissors cut paper, player 2 wins!!" << endl;
+    }
+    elseif((c1 == 'S' || c1 == 's') && (c2 == 'R' || c2 == 'r'))
+    {
+      cout << "Rock smashes scissor, player 2 wins!!" << endl;
+    }
+    elseif((c1 == 'S' || c1 == 's') && (c2 == 'P' || c2 == 'p'))
+    {
+      cout << "Scissors cut paper, player 1 wins!!" << endl;
+    }
+    else
+    {
+      cout << "Unrecognized entry, please try again." << endl;
       continue;
     }
     
     cout << "Would you like another round?" << endl
          << "Type Y for yes, else exit." << endl;
+    cin >> ans;
+    cin.ignore(100,'\n');
   } while(ans == 'Y' || ans == 'y');
   
   return 0;

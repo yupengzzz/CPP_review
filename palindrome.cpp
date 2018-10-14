7410449a6c78f77535d9426ef02fc44e4c0d0fe6
Yupeng Zhao <39612917+yupengzzz@users.noreply.github.com>
2018-10-14 11:14:17 -0700

    Create palindrome.cpp

---
+++ palindrome.cpp
@@ -0,0 +1,18 @@
+/*
+**Yupeng Zhao
+**2018-10-14
+**The palindrome function takes in a string of characters
+and returns the reverse of the string truncated at the end.
+The last letter of the string is not repeated.
+Example: "abc" -> "abcba" not "abccba"
+*/
+
+#include<iostream>
+using namespace std;
+
+char[] palindrome(char[] s);
+
+int main()
+{
+  return 0;
+}

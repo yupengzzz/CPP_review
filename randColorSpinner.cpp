64784fd767718cbcb733be182659680764f11edc
Yupeng Zhao <39612917+yupengzzz@users.noreply.github.com>
2018-11-22 16:41:34 -0800

    Update randColorSpinner.cpp

--- randColorSpinner.cpp
+++ randColorSpinner.cpp
@@ -16,16 +16,19 @@
 //		Preconditions: n is assigned a value between 1 and 5
 //		Postconditions: none
 //		Returns: the color of the spinner
+//
+// void autoSpin()
+//		Preconditions: spin() is defined, and int-color correspondence is defined
+//		Postconditions: all local variables in autoSpin() are destroyed after call
 
 #include<iostream>
 #include<string>
 #include<stdlib.h> //srand, rand
-#include<time.h> //time
 using namespace std;
 
 int spin(int n)
 {
-	srand(time(NULL));
+	srand(n);
 	n = rand() % 5 + 1;
 	return n;
 }
@@ -48,6 +51,38 @@ string intToColor(int n)
 	return("0");
 }
 
+void autoSpin()
+{
+	int i = 0; //counting number of times spinned
+	int m = 0; //user entered number for times
+	int a = 0;
+	int red = 0, green = 0, blue = 0, yellow = 0, orange = 0;
+
+	cout << "Enter the number of times for color spin: ";
+	cin >> m;
+	if(cin.fail())
+		exit (1);
+
+	for(i = 0; i < m; i++)
+	{
+		a = spin(a);
+		if (a == 1)
+			red++;
+		else if (a == 2)
+			green++;
+		else if (a == 3)
+			blue++;
+		else if (a == 4)
+			yellow++;
+		else if (a == 5)
+			orange++;
+	}
+
+	cout << "In a total of " << m << " spins, " << red << " are red," << endl
+		<< green << " are green, " << blue << " are blue, " << yellow
+		<< " are yellow, and " << orange << " are orange." << endl;
+}
+
 int main()
 {
 	int n = 0;
@@ -67,6 +102,8 @@ int main()
 		color = intToColor(n);
 		cout << "The color spinned is " << color << endl;
 	}while(1);
+
+	autoSpin();
 
 	return 0;
 }
